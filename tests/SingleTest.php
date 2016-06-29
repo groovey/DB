d92daa05f57ef16eba939889f9b1e7b3ab39e742
pokoot <pokoot@gmail.com>
2016-06-30 04:21:10 +0800

    Typo filename

--- tests/SingleTest.php
+++ tests/SingleTest.php
@@ -3,7 +3,7 @@
 use Silex\Application;
 use Groovey\ORM\Providers\ORMServiceProvider;
 
-class singleTest extends PHPUnit_Framework_TestCase
+class SingleTest extends PHPUnit_Framework_TestCase
 {
     private function init()
     {
